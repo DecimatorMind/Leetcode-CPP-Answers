1a54e80e765f2889d28788cddd6e7acb9ad5016d
Pranjal Bhardwaj <Decimatormind@gmail.com>
2020-07-26 15:44:34 +0530

    Initial Commit

---
+++ Leetcode CPP Answers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Leetcode CPP Answers
+//
+//  Created by Pranjal Bhardwaj on 26/07/20.
+//  Copyright © 2020 Pranjal Bhardwaj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
