eca803a0de78f70f2f6e2a7b59be21271ce2c0a2
Pranjal Bhardwaj <Decimatormind@gmail.com>
2020-12-03 00:06:44 +0530

    Create Main.cpp

---
+++ Leetcode CPP Answers/Main.cpp
@@ -0,0 +1,15 @@
+//
+//  Main.cpp
+//  Leetcode CPP Answers
+//
+//  Created by Pranjal Bhardwaj on 22/11/20.
+//  Copyright © 2020 Pranjal Bhardwaj. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    return 0;
+}
