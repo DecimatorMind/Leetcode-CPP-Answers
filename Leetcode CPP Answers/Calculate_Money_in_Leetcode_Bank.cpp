7718d82a343de356820439551b8489e141ff911d
Pranjal Bhardwaj <decimatormind@gmail.com>
2021-01-19 13:06:38 +0530

    Create Calculate_Money_in_Leetcode_Bank.cpp

---
+++ Leetcode CPP Answers/Calculate_Money_in_Leetcode_Bank.cpp
@@ -0,0 +1,17 @@
+//
+//  Calculate_Money_in_Leetcode_Bank.cpp
+//  Leetcode CPP Answers
+//
+//  Created by Pranjal Bhardwaj on 19/01/21.
+//  Copyright © 2021 Pranjal Bhardwaj. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int totalMoney(int n) {
+        return 0;
+    }
+};
